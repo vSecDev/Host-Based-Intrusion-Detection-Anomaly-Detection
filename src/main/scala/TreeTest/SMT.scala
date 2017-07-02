61381293b68b6017377ee759771248451439bc70
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2017-07-02 23:57:44 +0100

    SMT node to do

--- src/main/scala/TreeTest/SMT.scala
+++ src/main/scala/TreeTest/SMT.scala
@@ -3,12 +3,12 @@ package TreeTest
 /**
   * Created by Case on 02/07/2017.
   */
-abstract class SMT(val MAX_DEPTH: Int, val MAX_PHI: Int, val EXPAND_SEQUENCE_COUNT: Int){
+abstract class SMT(MAX_DEPTH: Int, MAX_PHI: Int, EXPAND_SEQUENCE_COUNT: Int)
 
-  case class Node()
-  case class Wildcard()
-  case class SequenceList(list: List[Sequence])
-}
+  case class Node(val MAX_DEPTH: Int, val MAX_PHI: Int, val EXPAND_SEQUENCE_COUNT: Int, val key: Int,  var children: List[SMT]) extends SMT(MAX_DEPTH, MAX_PHI, EXPAND_SEQUENCE_COUNT){}
+  //case class Wildcard(val MAX_DEPTH: Int, val MAX_PHI: Int, val EXPAND_SEQUENCE_COUNT: Int, val phi: Int) extends SMT(MAX_DEPTH, MAX_PHI, EXPAND_SEQUENCE_COUNT){}
+  case class SequenceList(val MAX_DEPTH: Int, val MAX_PHI: Int, val EXPAND_SEQUENCE_COUNT: Int, list: List[Sequence]) extends SMT(MAX_DEPTH, MAX_PHI, EXPAND_SEQUENCE_COUNT){}
+
 
 
 
--- src/test/scala/TreeTest/TreeTest.scala
+++ src/test/scala/TreeTest/TreeTest.scala
@@ -12,13 +12,16 @@ class TreeTest extends FunSuite{
   val intListTrace = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 1, 2, 3, 12, 13, 4, 5, 6, 7, 8, 9, 10, 11, 1, 2, 3, 14, 13, 4, 5, 6, 7, 8, 9, 10, 11, 1, 2, 3, 15, 4, 5, 6, 7, 8, 9, 10, 11, 1, 2, 3, 15)
 
   test("Node Test"){
-    val n = new Node(1,2,3)
+    val n = new Node(1,2,3,4, ())
     println("max depth: " + n.MAX_DEPTH)
     println("max phi: " + n.MAX_PHI)
-    println("expand count:" + n.EXPAND_SEQUENCE_COUNT)
+    println("expand count: " + n.EXPAND_SEQUENCE_COUNT)
+    println("node key: " + n.key)
+
 
     assert(n.MAX_DEPTH == 1)
     assert(n.MAX_PHI == 2)
     assert(n.EXPAND_SEQUENCE_COUNT == 3)
+    assert(n.key == 4)
   }
 }
