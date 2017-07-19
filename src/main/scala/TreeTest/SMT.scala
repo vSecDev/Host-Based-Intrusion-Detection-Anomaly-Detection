ca194ebead783f187c2756755498a5c83deec8bc
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2017-07-19 16:12:48 +0100

    SequenceList, growTree ver1 done

--- src/main/scala/TreeTest/SMT.scala
+++ src/main/scala/TreeTest/SMT.scala
@@ -12,8 +12,9 @@ import scala.collection.mutable.Map
   */
 abstract class SMT[A,B](maxDepth: Int, maxPhi: Int, maxSeqCount: Int)
 
-  case class Node[A,B](maxDepth: Int, maxPhi: Int, maxSeqCount: Int) extends SMT(maxDepth, maxPhi, maxSeqCount){
+  case class Node[A,B](maxDepth: Int, maxPhi: Int, maxSeqCount: Int) extends SMT(maxDepth, maxPhi, maxSeqCount) {
 
+    //A root node has no key. Root.getKey = None
     private var key: Option[A] = None
     private var eventCount = 0
     private var events: Map[B, Int] = Map[B, Int]()
@@ -23,21 +24,24 @@ abstract class SMT[A,B](maxDepth: Int, maxPhi: Int, maxSeqCount: Int)
 
 
     //TODO CHECK TYPE OF THIS VARIABLE. FIND OPTIMAL TYPE.
-    private var childrenGroup: Vector[Vector[SMT[_ <: A, _ <: B]]] = Vector[Vector[SMT[A,B]]]()
+    private var childrenGroup: Vector[Vector[SMT[_ <: A, _ <: B]]] = Vector[Vector[SMT[A, B]]]()
 
     def getKey: Option[A] = key
+
     def setKey(aKey: A): Unit = key match {
       case None => key = Option(aKey)
       case _ => throw new IllegalStateException("Node key cannot be reset")
     }
+
     def getEventCount: Int = eventCount
+
     def updateEvents(newEvent: B): Unit = {
 
       //update events to keep count on how many times this input has been seen
-        events.get(newEvent) match {
-          case None => events += (newEvent -> 1)
-          case Some(event) => events.update(newEvent, events(newEvent) + 1)
-        }
+      events.get(newEvent) match {
+        case None => events += (newEvent -> 1)
+        case Some(event) => events.update(newEvent, events(newEvent) + 1)
+      }
 
       //update event count to keep track of number of overall observations
       eventCount += 1
@@ -46,18 +50,21 @@ abstract class SMT[A,B](maxDepth: Int, maxPhi: Int, maxSeqCount: Int)
 
     def updatePredictions(): Unit = {
 
-      if (eventCount > 0){
+      if (eventCount > 0) {
 
-        for ((k,v) <- events){
-          if(predictions.contains(k))
+        for ((k, v) <- events) {
+          if (predictions.contains(k))
             predictions.update(k, v.toDouble / eventCount)
           else
-            predictions += (k -> (1.00/eventCount))
+            predictions += (k -> (1.00 / eventCount))
         }
       }
     }
+
     def getPredictions: Map[B, Double] = predictions
+
     def getChildren: Vector[Vector[SMT[_ <: A, _ <: B]]] = childrenGroup
+
     def getEvents: Map[B, Int] = events
 
     def getProbability(input: B): Option[Double] = predictions.get(input)
@@ -67,45 +74,49 @@ abstract class SMT[A,B](maxDepth: Int, maxPhi: Int, maxSeqCount: Int)
     //TODO SO MAKE SURE THE SEQUENCE THAT COULD NOT BE ADDED (BECAUSE UPDATESEQUENCES RETURNED FALSE) IS ADDED AFTER THE SPLIT!!!!!
     def growTree(condition: Vector[A], event: B): Unit = {
       //TODO CHECK FOR condition == Nil here???
-      if(maxDepth > 0) for{
+      if (maxDepth > 0) for {
         i <- 0 to maxPhi
         if condition.length > i
       } {
-        val newCondition = condition.drop(i)
+        //if current node is NOT root, the first element is also taken
+        //var newCondition = condition.drop(i)
+
         if (childrenGroup.size > i) childrenGroup(i)(0) match {
           case sl: SequenceList =>
-            sl.updateSequences((condition, event)) match {
-              case Some(x) => childrenGroup.updated(i, x)
+            sl.updateSequences((condition.drop(i), event)) match {
+              case Some(x) => childrenGroup.updated(i, x) // TODO CHECK IF UPDATESEQUENCES, REMOVES THE HEAD/CORRECT NUMBER OF ELEMENTS FROM CONDITION
               case None =>
             }
           case node: Node =>
-            val nextNode: Option[Node[A, B]] = childrenGroup(i).asInstanceOf[Vector[Node[A, B]]].find(x => x.getKey == condition.head)
+            val nextNode: Option[Node[A, B]] = childrenGroup(i).asInstanceOf[Vector[Node[A, B]]].find(x => x.getKey == condition.drop(i).head)
             nextNode match {
               case Some(x: Node[A, B]) =>
-                condition.tail match {
+                //there is a node where node.head = condition.drop(i).head
+                condition.drop(i).tail match {
                   case y +: ys => x.growTree(y +: ys, event)
                   case _ => println("should not ever get here with static window size"); updateEvents(event)
                 }
               case None =>
-              // TODO - THIS BIT ISN'T FINISHED!!!
-              /*
-                var newNode: Node[A, B] = new Node(maxDepth - i - 1, maxPhi, maxSeqCount)
-                newNode.setKey(condition.head)
-                newNode.growTree()
+                // TODO - THIS BIT ISN'T FINISHED!!!
 
-                childrenGroup(i) :+ */
+                var newNode: Node[A, B] = new Node(maxDepth - i - 1, maxPhi, maxSeqCount)
+                newNode.setKey(condition.drop(i).head)
+
+                condition.drop(i).tail match {
+                  case y +: ys => newNode.growTree(y +: ys, event)
+                  case _ => println("should not ever get here with static window size"); updateEvents(event)
+                }
+                childrenGroup(i) :+ newNode
             }
         } else {
-          val newSeqList = new SequenceList[A,B](maxDepth-i-1, maxPhi, maxSeqCount)
-          newSeqList.updateSequences()
-          // (!childrenGroup.size > i)	//current node DOESN'T have a list of children in "childrenGroup" at index i
-
+          val newSeqList = new SequenceList[A, B](maxDepth - i - 1, maxPhi, maxSeqCount)
+          newSeqList.updateSequences((condition.drop(i), event))
+          childrenGroup.updated(i, Vector[SMT[_ <: A, _ <: B]](newSeqList))
         }
-
-      } else{
+      } else {
         //  (!maxDepth > 0)   //maxDepth is 0! here
-
-
+        println("maxDepth <= 0. We should never get here if maxDepth of the root is larger than 0.")
+        throw new IllegalStateException("SMT with maxDepth <= 0 has no predictions.")
       }
     }
   }
