ea75eeba477c518df6d1339eaa1cc9898a6ac517
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2017-07-23 00:22:44 +0100

    SMT, growTree works with 2 inputs w different keys different first system call

--- src/test/scala/TreeTest/SMTTest.scala
+++ src/test/scala/TreeTest/SMTTest.scala
@@ -805,6 +805,129 @@ class SMTTest extends FunSuite with BeforeAndAfterAll {
     println("\n\n-----\nAfter adding condition2|666")
     println(n1)
   }
+  test("SMT, maxDepth 3 - growTree called with two 'Vector size == 2 | event' - first element same"){
+    val n1 = new Node[Int, Int](3,1,1)
+    val condition = Vector(1,2)
+    val condition2 = Vector(1,4)
+    //val condition2 = Vector(3,4)
+    val event = 666
+
+    n1.growTree(condition, event)
+    n1.getKey shouldBe None
+    assert(n1.getEventCount == 0)
+    assert(n1.getEvents.isEmpty)
+    assert(n1.getPredictions.isEmpty)
+    n1.getPredictions.get(event) shouldBe None
+    assert(n1.getChildren.size == 2)
+    assert(n1.getChildren(0).size == 1)
+    assert(n1.getChildren(1).size == 1)
+    n1.getChildren(0)(0) shouldBe a [SequenceList[_,_]]
+    n1.getChildren(1)(0) shouldBe a [SequenceList[_,_]]
+
+    println("After adding first condition")
+    println(n1)
+
+
+    val child0: SequenceList[Int, Int] = n1.getChildren(0)(0).asInstanceOf[SequenceList[Int, Int]]
+    val child1: SequenceList[Int, Int] = n1.getChildren(1)(0).asInstanceOf[SequenceList[Int, Int]]
+
+    for(i <- n1.getChildren.indices){
+      val child = n1.getChildren(i)(0).asInstanceOf[SequenceList[Int, Int]]
+      assert(child.getKeys.size == 1)
+      assert(child.getKeys(0) == condition.drop(i))
+      assert(child.getSequence(condition.drop(i)).get.getKey == condition.drop(i))
+      assert(child.getSequence(condition.drop(i)).get.getEventCount == 1)
+      assert(child.getSequence(condition.drop(i)).get.getEvents.size == 1)
+      assert(child.getSequence(condition.drop(i)).get.getEvents(event) == 1)
+      assert(child.getSequence(condition.drop(i)).get.getPredictions.size == 1)
+      assert(child.getSequence(condition.drop(i)).get.getPredictions(event) == 1.00)
+    }
+
+    println("before adding cond 2")
+    n1.growTree(condition2, event)
+
+    println("after adding cond 2")
+    println(n1)
+
+
+    n1.getKey shouldBe None
+    assert(n1.getEventCount == 0)
+    assert(n1.getEvents.isEmpty)
+    assert(n1.getPredictions.isEmpty)
+    n1.getPredictions.get(event) shouldBe None
+    assert(n1.getChildren.size == 2)
+    assert(n1.getChildren(0).size == 1)
+    assert(n1.getChildren(1).size == 1)
+    n1.getChildren(0)(0) shouldBe a [Node[_,_]]
+    n1.getChildren(1)(0) shouldBe a [SequenceList[_,_]]
+
+    for(i <- n1.getChildren.indices) {
+      val c = n1.getChildren(i)(0)
+
+      c match {
+        case child: SequenceList[Int, Int] => {
+        //  assert(child.getKeys.size == 2)
+
+          println("\n\n\n\n\n\n\n\n\n-------KEYS-------" )
+          for(k <- child.getKeys) println(k)
+          println("-------KEYS-------\n\n\n\n\n\n\n\n\n")
+
+
+
+          assert(child.getKeys(0) == condition.drop(i))
+          assert(child.getKeys(1) == condition2.drop(i))
+          assert(child.getSequence(condition.drop(i)).get.getKey == condition.drop(i))
+          assert(child.getSequence(condition2.drop(i)).get.getKey == condition2.drop(i))
+          assert(child.getSequence(condition.drop(i)).get.getEventCount == 1)
+          assert(child.getSequence(condition2.drop(i)).get.getEventCount == 1)
+          assert(child.getSequence(condition.drop(i)).get.getEvents.size == 1)
+          assert(child.getSequence(condition2.drop(i)).get.getEvents.size == 1)
+          assert(child.getSequence(condition.drop(i)).get.getEvents(event) == 1)
+          assert(child.getSequence(condition2.drop(i)).get.getEvents(event) == 1)
+          assert(child.getSequence(condition.drop(i)).get.getPredictions.size == 1)
+          assert(child.getSequence(condition2.drop(i)).get.getPredictions.size == 1)
+          assert(child.getSequence(condition.drop(i)).get.getPredictions(event) == 1.00)
+          assert(child.getSequence(condition2.drop(i)).get.getPredictions(event) == 1.00)
+        }
+        case _: Node[Int,Int] => {
+          assert(n1.getChildren(i).size == 1)
+          n1.getChildren(i) shouldBe a [Vector[Node[_,_]]]
+
+          for(j <- n1.getChildren(i).indices){
+            val curr = n1.getChildren(i)(j).asInstanceOf[Node[_,_]]
+
+            if(j == 0) {
+              assert(curr.getKey.get == condition.drop(i)(0))
+              assert(curr.getKey.get == condition2.drop(i)(0))
+              assert(curr.getChildren.size == 1)
+              val currSeq = curr.getChildren(0)(0).asInstanceOf[SequenceList[Int, Int]].sequences
+              assert(currSeq.size == 2)
+              assert(currSeq(0).getKey(0) == 2)
+              assert(currSeq(1).getKey(0) == 4)
+              assert(currSeq(0).getPredictions.size == 1)
+              assert(currSeq(1).getPredictions.size == 1)
+              assert(currSeq(0).getProbability(666).get == 1.00)
+              assert(currSeq(1).getProbability(666).get == 1.00)
+              assert(currSeq(0).getProbability(777).getOrElse(0.00) == 0.00)
+              assert(currSeq(1).getProbability(777).getOrElse(0.00) == 0.00)
+            }
+            else if(j == 1) {
+              assert(curr.getKey.get == condition2.drop(i)(0))
+              assert(curr.getChildren.size == 1)
+              val currSeq = curr.getChildren(0)(0).asInstanceOf[SequenceList[Int, Int]].sequences
+              assert(currSeq.size == 1)
+              assert(currSeq(0).getKey(0) == 4)
+              assert(currSeq(0).getPredictions.size == 1)
+              assert(currSeq(0).getProbability(666).get == 1.00)
+              assert(currSeq(0).getProbability(777).getOrElse(0.00) == 0.00)
+            }
+          }
+        }
+      }
+    }
+    //println("\n\n-----\nAfter adding condition2|666")
+    //println(n1)
+  }
  /* test("asdfsfd"){
     var childrenGroup: Vector[Vector[SMT[_ <: Int, _ <: Int]]] = Vector[Vector[SMT[Int, Int]]]()
     childrenGroup = childrenGroup :+ Vector(SequenceList[Int, Int](11,11,11))
