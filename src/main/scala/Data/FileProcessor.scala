1b63a051d082f600ccab695963016a6ad7b3bdea
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2017-08-06 19:14:17 +0100

    FileProcessor - modified preprocess logic - to test

--- src/main/scala/Data/FileProcessor.scala
+++ src/main/scala/Data/FileProcessor.scala
@@ -5,49 +5,63 @@ import java.io.BufferedWriter
 import java.io.FileWriter
 import java.nio.file.{Files, Paths}
 import scala.collection.mutable
+import scala.util.{Try, Success, Failure}
 import org.apache.commons.io.FilenameUtils
+import org.apache.commons.io.FileUtils
 
 
-class FileProcessor(source: File, target: File, delimiter: String) extends DataProcessor {
+class FileProcessor(source: File, target: File, delimiters: Array[String] = Array("\\s"), extensions: Array[String] = Array[String]()) extends DataProcessor {
+
+  require(source.exists && source.isDirectory, "Source directory does not exist or is not a directory!")
+  require(target.exists && target.isDirectory, "Target directory does not exist or is not a directory!")
 
   override def configure(): Unit = {}
-
-/*  override def preprocess(target: File): Unit = {
-
-    var sysCallSet = mutable.Set[String]()
-    var sysCallMap = Map[String, Int]()
-
-    println("sysCallSet.size BEFORE: " + sysCallSet.size)
-    val parentDir = new File(source)
-
-    fileStreamNoDirs(parentDir).foreach(f => sysCallSet = fileToSet(f, sysCallSet))
-    sysCallMap = sysCallSet.zipWithIndex.map { case (v, i) => (v, i + 1) }.toMap
-
-    fileStreamNoDirs(parentDir).foreach(f => toIntTrace(f, target, sysCallMap))
-  }*/
 
   //TODO - TRY/CATCH BLOCKS / EXCEPTION HANDLING TO ADD FOR IO OPS
   override def preprocess(): Unit = {
 
+    if(target.listFiles.nonEmpty) {
+      target.listFiles.foreach(f => FileUtils.deleteDirectory(f))
+    }
+
     var sysCallSet = mutable.Set[String]()
     var sysCallMap = Map[String, Int]()
 
-    println("sysCallSet.size BEFORE: " + sysCallSet.size)
-    val parentDir = new File(source)
+    //val parentDir = source
 
-    fileStreamNoDirs(parentDir).foreach(f => sysCallSet = fileToSet(f, sysCallSet))
+    fileStreamNoDirs(source).foreach(f => sysCallSet = fileToSet(f, sysCallSet))
     println("sysCallSet.size AFTER: " + sysCallSet.size)
 
     sysCallMap = sysCallSet.zipWithIndex.map { case (v, i) => (v, i + 1) }.toMap
     println("sysCallMap.size AFTER (should be same as sysCallSet.size): " + sysCallMap.size)
 
-    fileStream(parentDir).foreach(f => {
-      if(f.isDirectory){
-        println("new dir will be created here: " + (target + f.getCanonicalPath.replaceAll(source.getCanonicalPath, "") + "_INT"))
-       // val path = Paths.get(target + f.getName + "_INT")
-       // Files.createFile(path)
-      }else {
-       // toIntTrace(f, target, sysCallMap)
+    fileStream(source).foreach(f => {
+      //val path = target.getCanonicalPath + f.getCanonicalPath.replace(source.getCanonicalPath, "")
+
+
+      if (f.isDirectory) {
+        println("\ntarget.getCanonicalPath: " + target.getCanonicalPath)
+        println("source.getCanonicalPath: " + source.getCanonicalPath)
+        println("f.getCanonicalPath: " + f.getCanonicalPath)
+        println("f.getCanonicalPath.replace(source.getCanonicalPath: " + f.getCanonicalPath.replace(source.getCanonicalPath, ""))
+
+        println("new dir will be created here: " + (target.getCanonicalPath + f.getCanonicalPath.replace(source.getCanonicalPath, "")))
+
+        val path = target.getCanonicalPath + f.getCanonicalPath.replace(source.getCanonicalPath, "")
+        println("\npath: " + path)
+        Files.createDirectory(Paths.get(path))
+        println()
+        println()
+      } else {
+        val path = target.getCanonicalPath + FilenameUtils.removeExtension(f.getCanonicalPath.replace(source.getCanonicalPath, "")) + "_INT.IDS"
+        println("\npath: " + path)
+        val intTrace = toIntTrace(f, sysCallMap)
+        val newFile = new File(path)
+
+
+        val bw = new BufferedWriter(new FileWriter(newFile))
+        bw.write(intTrace)
+        bw.close()
       }
     })
   }
@@ -75,17 +89,14 @@ class FileProcessor(source: File, target: File, delimiter: String) extends DataP
     val source = scala.io.Source.fromFile(f)
     val lines = try source.getLines mkString "\n" finally source.close()
     //val vec: Vector[String] = lines.split("\\s+").map(_.trim).toVector
-    val vec: Vector[String] = lines.split(delimiter).map(_.trim).toVector
+    val vec: Vector[String] = lines.split(delimiters.mkString("|")).map(_.trim).toVector
     set ++= vec
   }
 
-  private def toIntTrace(f: File, target: File, map: Map[String, Int]): File = {
+  private def toIntTrace(f: File, map: Map[String, Int]): String = {
     val source = scala.io.Source.fromFile(f)
     val lines = try source.getLines mkString "\n" finally source.close()
     val result = map.foldLeft(lines)((a, b) => a.replaceAllLiterally(b._1, b._2.toString))
-    val newFile = new File(target, FilenameUtils.removeExtension(f.getName) + "_INT.IDS")
-    val bw = new BufferedWriter(new FileWriter(newFile))
-    bw.write(result)
-    bw.close()
+   result
   }
 }
--- src/test/scala/Data/FileProcessorTest.scala
+++ src/test/scala/Data/FileProcessorTest.scala
@@ -12,8 +12,10 @@ class FileProcessorTest extends FunSuite {
   test("FileProcessor - preprocess creates target dirs"){
     val s = new File(testSource)
     val t = new File(testTarget)
-    val d = "\\s"
-    val fp = new FileProcessor(s,t,d)
+    val d = Array("\\s")
+    val e = Array("GHC")
+
+    val fp = new FileProcessor(s,t,d,e)
     fp.preprocess()
 
   }
