19bdeaaf3b135a7831e02300ea4a40f519494129
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2017-08-08 12:42:58 +0100

    Added DataModel trait for saving/loading trained decision engine models

---
+++ src/main/scala/Data/DataModel.scala
@@ -0,0 +1,8 @@
+package Data
+
+/**
+  * Created by apinter on 08/08/2017.
+  */
+trait DataModel {
+
+}
--- src/main/scala/Data/DataProcessor.scala
+++ src/main/scala/Data/DataProcessor.scala
@@ -1,9 +1,11 @@
 package Data
 
-import scala.io.BufferedSource
+import java.io.File
 
 trait DataProcessor {
   def configure(): Unit
   def preprocess(): Option[Map[String,Int]]
   def getData(wrapper: DataWrapper): Option[DataWrapper]
+  def saveModel(model: DataModel): Boolean
+  def loadModel(source: File): Option[DataModel]
 }
