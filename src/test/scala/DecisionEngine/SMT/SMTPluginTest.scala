bb3be4e48a43c93abad5c1ab683a610c37a98fb7
BBK-PiJ-2015-07 <devaccpub@gmail.com>
2017-08-12 21:05:13 +0100

    SMTPlugin - began testing learn method

--- src/test/scala/DecisionEngine/SMT/SMTPluginTest.scala
+++ src/test/scala/DecisionEngine/SMT/SMTPluginTest.scala
@@ -2,6 +2,7 @@ package DecisionEngine.SMT
 
 import java.io._
 
+import Data.{DataModel, DataWrapper, StringDataWrapper}
 import Data.File.FileProcessor
 import DecisionEngine.DecisionEngineConfig
 import DecisionEngine.SMT.{Node, SequenceList}
@@ -65,6 +66,62 @@ class SMTPluginTest extends  FunSuite {
     assert(root.smoothing == smoothing)
     assert(root.prior == prior)
   }
+  test("SMTPlugin - learn returns trained model"){
+
+    val maxDepth = 4
+    val maxPhi = 2
+    val maxSeqCount = 1
+    val smoothing = 1.0
+    val prior = 1.0
+    val ints = true
+    val condition1 = Vector(1, 2, 3)
+    val condition2 = Vector(4, 5, 6)
+    val condition3 = Vector(7, 8, 9)
+    val event1 = 666
+    val event2 = 777
+    val event3 = 888
+
+    val n1 = Node[Int, Int](maxDepth, maxPhi, maxSeqCount, smoothing, prior)
+    n1.learn(condition1, event1)
+    var r1 = n1.predict(condition1, event1)
+    var r2 = n1.predict(condition1, event2)
+    assert(r1._1 == 4.0 && r1._2 == 2.0)
+    assert(r2._1 == 2.0 && r2._2 == 2.0)
+    assert(n1.getChildren(0)(0).asInstanceOf[SequenceList[Int, Int]].getSequence(condition1).get.getWeight == 2.0 / 3)
+
+    val model = new DataModel
+    model.store(n1)
+    assert(model.retrieve.get.isInstanceOf[Node[Int,Int]])
+
+    val plugin = new SMTPlugin
+    assert(plugin.loadModel(model))
+    val returnedModel1 = plugin.getModel.get.retrieve.get
+    assert(returnedModel1.isInstanceOf[Node[Int, Int]])
+    val retNode1 = returnedModel1.asInstanceOf[Node[Int, Int]]
+
+    var r3 = retNode1.predict(condition1, event1)
+    var r4 = retNode1.predict(condition1, event2)
+    assert(r3._1 == 4.0 && r1._2 == 2.0)
+    assert(r4._1 == 2.0 && r2._2 == 2.0)
+    assert(retNode1.getChildren(0)(0).asInstanceOf[SequenceList[Int, Int]].getSequence(condition1).get.getWeight == 2.0 / 3)
+
+
+    val dw = new StringDataWrapper
+    //storing condition1, event2
+    dw.store("1 2 3 777")
+    plugin.learn(Vector(dw), None, ints)
+    val returnedModel2 = plugin.getModel.get.retrieve.get
+    val retNode2 = returnedModel2.asInstanceOf[Node[Int, Int]]
+
+    val r5 = retNode2.predict(condition1, event1)
+    val r6 = retNode2.predict(condition1, event2)
+    var r7 = retNode2.predict(condition1, event3)
+    assert(retNode2.getChildren(0)(0).asInstanceOf[SequenceList[Int, Int]].getSequence(condition1).get.getWeight == 2.0 / 3)
+    assert(r5._1 == 2.0 && r5._2 == 2.0)
+    assert(r6._1 == 2.0 && r6._2 == 2.0)
+    assert(r7._1 == 1.0 && r7._2 == 2.0)
+  }
+
 
 }
 
